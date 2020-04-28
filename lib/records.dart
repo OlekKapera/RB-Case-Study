40c58081368d126343acecf69e871c56385385e6
Aleksander Arkadiusz Kapera <a.kapera@stud.elka.pw.edu.pl>
2020-04-28 18:04:32 +0200

    Added custom gradient button

--- lib/records.dart
+++ lib/records.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_icons_icons.dart';
+import 'widgets/gradient_button.dart';
 
 class Records extends StatelessWidget {
   @override
@@ -20,20 +21,35 @@ class Records extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          SizedBox(height: 32,),
-          CircleAvatar(
-            backgroundColor: Colors.yellow[800],
-            radius: 22,
-            child: Icon(
-              CustomIcons.moon,
-              color: Colors.white,
-              size: 20,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(32, 32, 32, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.yellow[800],
+              radius: 22,
+              child: Icon(
+                CustomIcons.moon,
+                color: Colors.white,
+                size: 20,
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 16),
+            Text(
+              'Get to know your baby\'s sleep patterns and keep track of how much sleep they are getting here',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey[500],
+                fontSize: 15,
+                backgroundColor: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 24),
+            GradientButton('Add new sleeping record'),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/gradient_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class GradientButton extends StatelessWidget {
+
+  static const double radius = 64.0;
+
+  final String _buttonText;
+  GradientButton(this._buttonText);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: () {},
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(radius),
+      ),
+      padding: const EdgeInsets.all(0.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(radius)),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                _buttonText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
